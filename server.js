73aeb3a9935feaf1fdf76c23bc6790967c09dc6b
Abby Westphal <47372310+a-westphal@users.noreply.github.com>
2019-10-28 18:10:32 -0500

    Got global variables working for State and Year
    global variables in state and year HTML work, yielding a graph

--- server.js
+++ server.js
@@ -43,16 +43,33 @@ app.get('/', (req, res) => {
     ReadFile(path.join(template_dir, 'index.html')).then((template) => {
         let response = template;
 		var stringHold = "";
+    var coal =0;
+    var gas =0;
+    var nuclear =0;
+    var petroleum =0;
+    var renew =0;
+
 		var replacePromise= new Promise((resolve,reject)=>{
-			db.all("SELECT * FROM Consumption ORDER BY year", (err,rows) =>{
+			db.all("SELECT * FROM Consumption WHERE year = 2017", (err,rows) =>{
 				rows.forEach(function (row) {
 					stringHold=stringHold+"<tr>"+"<td>"+row.state_abbreviation+"</td>" +"<td>"+row.coal+"</td>"+"<td>"+row.natural_gas+"</td>"+"<td>"+row.nuclear+"</td>"+"<td>"+row.petroleum+"</td>"+"<td>"+row.renewable+"</td>"+"</tr>";
+          coal = coal + row.coal;
+          gas = gas + row.natural_gas;
+          nuclear = nuclear + row.nuclear;
+          petroleum = petroleum + row.petroleum; 
+          renew = renew = row.renewable; 
+
 					})
 					resolve(stringHold);
 			});
 		})
 			replacePromise.then(data=>{
 				response=response.replace("replace",data);
+        response=response.replace("var coal_count;", "var coal_count = "+ coal + ";");
+        response=response.replace("var natural_gas_count;", "var natural_gas_count = "+ gas + ";");
+        response=response.replace("var nuclear_count;", "var nuclear_count = " + nuclear + ";");
+        response=response.replace("var petroleum_count;", "var petroleum = "+petroleum + ";");
+        response=response.replace("var renewable_count;", "var renewable_count = "+ renew + ";");
 				WriteHtml(res, response);
 			});
 			
@@ -67,9 +84,20 @@ app.get('/year/:selected_year', (req, res) => {
         let response = template;
 		let year = req.url.substring(6);
 		var stringHold = "";
+    var coal =0;
+    var gas =0;
+    var nuclear =0;
+    var petroleum =0;
+    var renew =0;
+
 		var replacePromise= new Promise((resolve,reject)=>{
 			db.all('SELECT * FROM Consumption WHERE year = ? ORDER BY state_abbreviation',[year],(err,rows) =>{
 				rows.forEach(function (row) {
+          coal = coal + row.coal;
+          gas = gas + row.natural_gas;
+          nuclear = nuclear + row.nuclear;
+          petroleum = petroleum + row.petroleum; 
+          renew = renew = row.renewable; 
 					total=row.coal+row.natural_gas+row.nuclear+row.petroleum+row.renewable;
 					stringHold=stringHold+"<tr>"+"<td>"+row.state_abbreviation+"</td>" +"<td>"+row.coal+"</td>"+"<td>"+row.natural_gas+"</td>"+"<td>"+row.nuclear+"</td>"+"<td>"+row.petroleum+"</td>"+"<td>"+row.renewable+"</td>"+"<td>"+total+"</td>"+"</tr>";
 					})
@@ -79,6 +107,13 @@ app.get('/year/:selected_year', (req, res) => {
 			replacePromise.then(data=>{
 				response=response.replace("replace",data);
 				response= response.replace("In Depth Analysis", "In Depth Analysis of "+year);
+        response=response.replace("var year;" , "var year = " + year); 
+        response=response.replace("var coal_count;", "var coal_count = "+ coal + ";");
+        response=response.replace("var natural_gas_count;", "var natural_gas_count = "+ gas + ";");
+        response=response.replace("var nuclear_count;", "var nuclear_count = " + nuclear + ";");
+        response=response.replace("var petroleum_count;", "var petroleum = "+petroleum + ";");
+        response=response.replace("var renewable_count;", "var renewable_count = "+ renew + ";");
+
 				WriteHtml(res, response);
 			});
     }).catch((err) => {
@@ -92,35 +127,44 @@ app.get('/state/:selected_state', (req, res) => {
         let response = template;
 		let state=req.url.substring(7);
 		let stringHold="";
-		let stateName =[];
-		let coal_counts=[];
-		let natural_counts=[];
-		let petroleum_counts=[];
-		let renewable_counts=[]; 
-		let nuclear_counts =[];
+		let stateName;
+		let coal_counts=[58];
+		let natural_counts=[58];
+		let petroleum_counts=[58];
+		let renewable_counts=[58]; 
+		let nuclear_counts =[58];
 
 		var replacePromise= new Promise((resolve,reject)=>{
 			db.each('SELECT * FROM States WHERE state_abbreviation = ?',[state],(err,rows)=>{stateName= rows.state_name});
 			db.all('SELECT * FROM Consumption WHERE state_abbreviation = ? ORDER BY year',[state],(err,rows) =>{
-				rows.forEach(function (row) {
-					coal_counts.push(row.coal);
-					natural_counts.push(row.natural_gas);
-        			nuclear_counts.push(row.nuclear);
-        			petroleum_counts.push(row.petroleum);
-        			renewable_counts.push(row.renewable);
+				var count = 0;
+        rows.forEach(function (row) {
+
+					coal_counts[count]=row.coal;
+					natural_counts[count] = (row.natural_gas);
+        	nuclear_counts[count] = (row.nuclear);
+        	petroleum_counts[count] = (row.petroleum);
+        	renewable_counts[count] = (row.renewable);
 					total=row.coal+row.natural_gas+row.nuclear+row.petroleum+row.renewable;
 					stringHold=stringHold+"<tr>"+"<td>"+row.year+"</td>" +"<td>"+row.coal+"</td>"+"<td>"+row.natural_gas+"</td>"+"<td>"+row.nuclear+"</td>"+"<td>"+row.petroleum+"</td>"+"<td>"+row.renewable+"</td>"+"<td>"+total+"</td>"+"</tr>";
+          count = count + 1;
 					})
 					resolve(stringHold);
 			});
 		})
 			replacePromise.then(data=>{
+
 				response=response.replace("replace",data);
 				response=response.replace("Yearly Snapshot", "Yearly Snapshot of "+ stateName);
 				response= response.replace("In Depth Analysis", "In Depth Analysis of "+stateName);
 				image = "/images/"+state+".jpg";
 				response= response.replace("/images/noimage.jpg",image);
-				response= response.replace("var state;", "var state =" + state + ";");
+				response= response.replace("var state;", "var state = " + "\"" + stateName + "\""+ ";");
+        response=response.replace("var coal_counts;","var coal_counts = [" + coal_counts + "];");
+        response=response.replace("var natural_gas_counts;","var natural_gas_counts = [" + natural_counts + "];");
+        response=response.replace("var nuclear_counts;","var nuclear_counts = [" + nuclear_counts + "];");
+        response=response.replace("var petroleum_counts;","var petroleum_counts = [" + petroleum_counts + "];");
+        response=response.replace("var renewable_counts;","var renewable_counts = [" + renewable_counts + "];");
 				WriteHtml(res, response);
 			});
         // modify `response` here
@@ -136,19 +180,34 @@ app.get('/energy-type/:selected_energy_type', (req, res) => {
         let energy = req.url.substring(13);
         let stringHold = ""; 
         var data = {};
+        var variables = {};
         totals = new Array (58).fill(0);
 	   
   		var replacePromise = new Promise((resolve,reject)=> {
+        
   			db.all('SELECT * FROM Consumption ORDER BY year,state_abbreviation',(err,rows)=>{
+          var count = 0;
   				rows.forEach(function (row) {
   					//check if the year already exists (hasOwnProperty(key))
   					if(data.hasOwnProperty(row.year)!= true)
   					{
   						data[row.year] = new Object ();
   					}
+            if(variables.hasOwnProperty(row.state_abbreviation) != true)
+            {
+              variables[row.state_abbreviation] = new Array (58).fill(0);
+            }
   					data[row.year][row.state_abbreviation] = row[energy];
+            variables[row.state_abbreviation][count] = row[energy];
+            count = count +1;  
+            if (count == 58)
+            {
+              count = 0;
+            }
 
   				})
+
+          console.log(variables);
 
   				let k =0; 
   				for(let i =1960; i < 2018; i ++)
@@ -171,10 +230,22 @@ app.get('/energy-type/:selected_energy_type', (req, res) => {
   		})
 
         	replacePromise.then(data=>{
-        	  	response = response.replace("replace",data);
-        		response = response.replace("In Depth Analysis", "In Depth Analysis of " + energy + " energy");
-        		response = response.replace("Consumption Snapshot", "Consumption Snapshot of "+energy + " energy");
+        	  if(energy == "natural_gas")
+            { //just to get rid of the underscore 
+              response = response.replace("In Depth Analysis", "In Depth Analysis of natural gas");
+              response = response.replace("Consumption Snapshot","Consumption Snapshot of natural gas");
+              response = response.replace("<h1>US Energy Consumption!</h1>", "<h1>US Energy Consumption of natural gas</h1>" );
+              response = response.replace("var energy_type;", "var energy_type = \"natural gas\";"); 
+            }
+            else{
+              response = response.replace("In Depth Analysis", "In Depth Analysis of " + energy);
+              response = response.replace("Consumption Snapshot", "Consumption Snapshot of "+energy);
+              response = response.replace("<h1>US Energy Consumption!</h1>", "<h1>US Energy Consumption of " + energy + "</h1>" );
+              response = response.replace("var energy_type;", "var energy_type = " + "\"" + energy + "\"" +";"); 
+            }
+            response = response.replace("replace",data);
         		response = response.replace("images/noimage.jpg", "images/"+energy+".jpg");
+             console.log(energy);
         		WriteHtml(res,response);
         	});
     }).catch((err) => {
